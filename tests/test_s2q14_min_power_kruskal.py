2ea806d505cb4f3bdac914705719db9b06091f21
Clemence Chevrier <clemence.chevrier@ensae.fr>
2023-03-15 18:08:21 +0100

    fin

--- tests/test_s2q14_min_power_kruskal.py
+++ tests/test_s2q14_min_power_kruskal.py
@@ -1,6 +1,7 @@
 import sys 
 sys.path.append("delivery_network")
-from graph import Graph, UnionFind, kruskal, graph_from_file, min_power_kruskal
+from graph import Graph, UnionFind, graph_from_file, kruskal, min_power_kruskal, orienter_arbre
+import time
 
 
 g=Graph([1,2,3,4,5,6])
@@ -13,5 +14,27 @@ g.add_edge(3, 4, 9)
 g.add_edge(3, 5, 9)
 g.add_edge(4, 5, 5)
 
-print(min_power_kruskal(g, 1, 5))
+g1=kruskal(g)
+t_dep= time.perf_counter()
+h=orienter_arbre(g1)
+min_pwg, traj_g=min_power_kruskal(g1, h, 4, 2)
+t_fin= time.perf_counter()
+t=t_fin-t_dep
+
+print("Pour le petit graphe, la puissance minimale pour ce trajet est ", min_pwg,"et on passe par le chemin " , traj_g, "et le temps nécessaire est ", t , "sec")
+
 #c'est bien cohérent avec l'arbre de poids couvrant donné
+
+
+#test sur les grands graphes: 
+g=graph_from_file("input/network.2.in")
+g2=kruskal(g)
+t_dep= time.perf_counter()
+h=orienter_arbre(g2)
+min_pwg, traj_g=min_power_kruskal(g2, h, 1000, 30)
+t_fin= time.perf_counter()
+t=t_fin-t_dep
+print("Pour le grand graphe, la puissance minimale pour ce trajet est ", min_pwg,"et on passe par le chemin " , traj_g, "et le temps nécessaire est ", t , "sec")
+
+
+#le temps d'éxecution est raisonnable pour la version optimisée de max power kruskal
--- tests/test_s2q15_time_min_power.py
+++ tests/test_s2q15_time_min_power.py
@@ -3,9 +3,8 @@ import time
 sys.path.append("delivery_network")
 from graph import estimated_time_kruskal
 
+print("Le temps nécessaire est :",  estimated_time_kruskal(1), "s") 
+print("Le temps nécessaire est :",  estimated_time_kruskal(2), "s") 
+print("Le temps nécessaire est :",  estimated_time_kruskal(3), "s") 
 
-print("Le temps nécessaire est :",  estimated_time_kruskal(1), "s")
-
-#il y a deux problèmes :
-#1): si les graphes sont disjoints et qu'il ne peut pas y aller alors la fonction ne s'arrête pas
-#2): pour x>1, trop long pour "input/network.x.in"
+#Le temps d'exécution annoncé est très long mais la fonction qui estime la durée s'exécute en moins d'une minute, même pour les grands graphes
